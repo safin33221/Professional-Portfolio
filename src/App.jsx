512b2a58d8f8dbc327da9349d6b163a7bb26dc86
safin33221 <saifn33221@gmail.com>
2025-02-03 22:47:10 +0600

    completed to implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import './App.css'
+
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./Components";
 function App() {
 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+// import styles from '../styles';
 
+import { styles } from "../styles"
+
+
+// import CmputersCanvas from './canvas';
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto top-32'>
+      <div className="flex gap-5">
+        <div className={`${styles.paddingX} absolute inset-0  max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+          <div className="flex flex-col items-center justify-center mt-5">
+            <div className="w-5 h-5 rounded-full  bg-[#915eff]" />
+            <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#915EFF]'>Safin</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              and Fronend developer, user <br className='sm:block hidden' />
+              interfaces and web applications
+            </p>
+          </div>
+
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
